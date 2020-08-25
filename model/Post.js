93bc01d8db217a87371c0d187895d01bafc3935a
Mohit <mohitsingh11082002@gmail.com>
2020-08-25 17:53:33 +0530

    fixes.

--- model/Post.js
+++ model/Post.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const post_schema = mongoose.Schema({
     title: {
         type: String,
-        // required: true
+        required: true
     },
     content: {
         type: String,
-        // required: true
+        required: true
     },
     comments: [{
         type: mongoose.Schema.Types.ObjectId,
